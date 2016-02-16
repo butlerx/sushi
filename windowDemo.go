4b60ec4a1c8baceb9e0340a29e0fb8d64c6e7b15
Tail-wind <eannambyrne@gail.com>
2016-02-16 16:03:33 +0000

    multiple concurrent windows

--- windowDemo.go
+++ windowDemo.go
@@ -13,49 +13,29 @@ func main() {
 	}
 	defer browser.Close()
 
-	info := gocui.NewGui()
-	if err := info.Init(); err != nil {
-		log.Panicln(err)
-	}
-	defer info.Close()
-
-	browser.SetLayout(browserLayout)
+	browser.SetLayout(layout)
 	if err := browser.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
-		log.Panicln(err)
-	}
-
-	info.SetLayout(infoLayout)
-	if err := info.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
 		log.Panicln(err)
 	}
 
 	if err := browser.MainLoop(); err != nil && err != gocui.ErrQuit {
 		log.Panicln(err)
 	}
-
-	if err := info.MainLoop(); err != nil && err != gocui.ErrQuit {
-		log.Panicln(err)
-	}
 }
 
-func browserLayout(g *gocui.Gui) error {
+func layout(g *gocui.Gui) error {
 	maxX, maxY := g.Size()
-	if v, err := g.SetView("hello", 0, 0, maxX/2, maxY-1); err != nil {
+	if v, err := g.SetView("browser", 0, 0, maxX/2, maxY-1); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
-		fmt.Fprintln(v, "Hello world!")
+		fmt.Fprintln(v, "Browser")
 	}
-	return nil
-}
-
-func infoLayout(g *gocui.Gui) error {
-	maxX, maxY := g.Size()
-	if v, err := g.SetView("googbye", maxX/2, 0, maxX-1, maxY-1); err != nil {
+	if d, err := g.SetView("infopane", maxX/2, 0, maxX-1, maxY-1); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
-		fmt.Fprintln(v, "Goodbye world!")
+		fmt.Fprintln(d, "Infopane")
 	}
 	return nil
 }
