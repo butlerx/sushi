d14ad202209c2d8c6b8211f9faa2068bc3edda52
Tail-wind <eannambyrne@gmail.com>
2016-02-22 16:45:57 +0000

    tidyied up reading cmd args, started keybinding

--- windowDemo.go
+++ windowDemo.go
@@ -10,46 +10,92 @@ import (
 )
 
 func main() {
-	browser := gocui.NewGui()
-	if err := browser.Init(); err != nil {
+	window := gocui.NewGui()
+	if err := window.Init(); err != nil {
 		log.Panicln(err)
 	}
-	defer browser.Close()
+	defer window.Close()
 
-	browser.SetLayout(layout)
-	if err := browser.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+	window.SetLayout(layout)
+	if err := keybindings(window); err != nil {
 		log.Panicln(err)
 	}
+	window.Cursor = true
 
-	if err := browser.MainLoop(); err != nil && err != gocui.ErrQuit {
+	if err := window.MainLoop(); err != nil && err != gocui.ErrQuit {
 		log.Panicln(err)
 	}
 }
 
-func layout(g *gocui.Gui) error {
-	maxX, maxY := g.Size()
-	if v, err := g.SetView("browser", -1, -1, maxX/3, maxY); err != nil {
-		if err != gocui.ErrUnknownView {
-			return err
-		}
-		arg1 := "./"
-		if len(os.Args) != 1 {
-			arg1 = os.Args[1]
-		}
-		files, _ := ioutil.ReadDir(arg1)
-		for _, f := range files {
-			fmt.Fprintln(v, f.Name())
-		}
+func keybindings(g *gocui.Gui) error {
+	if err := g.SetKeybinding("browser", gocui.KeyArrowDown, gocui.ModNone, cursorDown); err != nil {
+		return err
 	}
-	if d, err := g.SetView("infopane", maxX/3, -1, maxX, maxY); err != nil {
-		if err != gocui.ErrUnknownView {
-			return err
-		}
-		fmt.Fprintln(d, "Infopane")
+
+	if err := g.SetKeybinding("browser", gocui.KeyArrowUp, gocui.ModNone, cursorUp); err != nil {
+		return err
+	}
+
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		return err
 	}
 	return nil
 }
 
+func layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+	arg1 := "./"           //by default, read current dir
+	if len(os.Args) != 1 { //check to see if you have cmd line args
+		arg1 = os.Args[1]
+	}
+	files, _ := ioutil.ReadDir(arg1)
+
+	if v, err := g.SetView("browser", -1, -1, maxX/3, maxY); err != nil { //draw left pane
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+
+		for _, f := range files { //print file names
+			fmt.Fprintln(v, f.Name())
+		}
+	}
+	if d, err := g.SetView("infopane", maxX/3, -1, maxX, maxY); err != nil { //draw right pane
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		fmt.Fprintln(d, "infopane")
+	}
+	return nil
+}
+
+//functions called by keypress below
+
 func quit(g *gocui.Gui, v *gocui.View) error {
 	return gocui.ErrQuit
+}
+
+func cursorDown(g *gocui.Gui, v *gocui.View) error {
+	if v != nil {
+		cx, cy := v.Cursor()
+		if err := v.SetCursor(cx, cy+1); err != nil {
+			ox, oy := v.Origin()
+			if err := v.SetOrigin(ox, oy+1); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func cursorUp(g *gocui.Gui, v *gocui.View) error {
+	if v != nil {
+		ox, oy := v.Origin()
+		cx, cy := v.Cursor()
+		if err := v.SetCursor(cx, cy-1); err != nil && oy > 0 {
+			if err := v.SetOrigin(ox, oy-1); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
 }
