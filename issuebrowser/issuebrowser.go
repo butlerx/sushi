945b41b391ab7b1770ad8d26696ec967fb358206
Tail-wind <eannambyrne@gmail.com>
2016-03-18 10:22:06 +0000

    Uncompleted changes to issue creation

--- issuebrowser/issuebrowser.go
+++ issuebrowser/issuebrowser.go
@@ -210,7 +210,7 @@ func Show() {
 	window.SelFgColor = gocui.ColorBlack
 	window.Cursor = true
 	timer := cron.New()
-	timer.AddFunc("0 5 * * * *", func() {
+	/*timer.AddFunc("0 5 * * * *", func() {
 		reason, subject, update := gitissue.WatchRepo(getRepo())
 		if update {
 			fmt.Print("\a")
@@ -221,7 +221,7 @@ func Show() {
 			notifications.Clear()
 			fmt.Fprintln(notifications, reason+": "+subject)
 		}
-	})
+	})*/
 	timer.AddFunc("0 5 * * * *", func() {
 		issueList = getIssues()
 		browser, err := window.View("browser")
@@ -336,9 +336,9 @@ func layout(g *gocui.Gui) error {
 
 func keybindings(g *gocui.Gui) error {
 	mainWindows := []string{"browser", "issuepane", "commentpane", "labelpane", "milestonepane", "assigneepane"}
-	displayWindows := []string{"issuepane", "commentpane", "labelpane", "milestonepane", "assigneepane", "helpPane", "labelBrowser", "labelRemover", "sortChoice", "filterChoice"}
-	controlWindows := []string{"browser", "issuepane", "commentpane", "labelpane", "milestonepane", "assigneepane", "helpPane", "labelBrowser", "labelRemover", "commentBrowser", "sortChoice", "filterChoice"}
-	dialogBoxes := []string{"helpPane", "labelBrowser", "labelRemover", "sortChoice", "filterChoice", "issueEd", "commentBody", "commentDeleter", "commentBrowser", "commentViewer"}
+	displayWindows := []string{"issuepane", "commentpane", "labelpane", "milestonepane", "assigneepane", "helpPane", "labelBrowser", "labelRemover", "sortChoice", "selectionPane", "filterChoice"}
+	controlWindows := []string{"browser", "issuepane", "commentpane", "labelpane", "milestonepane", "assigneepane", "helpPane", "labelBrowser", "labelRemover", "commentBrowser", "sortChoice", "selectionPane", "filterChoice"}
+	dialogBoxes := []string{"helpPane", "labelBrowser", "labelRemover", "sortChoice", "filterChoice", "issueEd", "selectionPane", "commentBody", "commentDeleter", "commentBrowser", "commentViewer"}
 
 	for i := 0; i < len(mainWindows); i++ {
 		if err := g.SetKeybinding(mainWindows[i], gocui.KeyF1, gocui.ModNone, help); err != nil {
@@ -374,6 +374,10 @@ func keybindings(g *gocui.Gui) error {
 		if err := g.SetKeybinding(dialogBoxes[i], gocui.KeyCtrlC, gocui.ModNone, cancel); err != nil {
 			return err
 		}
+	}
+
+	if err := g.SetKeybinding("selectionPane", gocui.KeyEnter, gocui.ModNone, nextEntry); err != nil {
+		return err
 	}
 
 	if err := g.SetKeybinding("commentBrowser", gocui.KeyArrowUp, gocui.ModNone, cursorupGetComments); err != nil {
@@ -2138,13 +2142,13 @@ func writeLabel(g *gocui.Gui, v *gocui.View) error {
 func newIssue(g *gocui.Gui, v *gocui.View) error {
 	previousView = v
 	maxX, maxY := g.Size()
-	if issueprompt, err := g.SetView("issueprompt", maxX/4, maxY/3, maxX-(maxX/4), (maxY/3)+(maxY/6)); err != nil {
+	if issueprompt, err := g.SetView("issueprompt", maxX/4, maxY/6, maxX-(maxX/4), maxY/3); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
 		fmt.Fprintln(issueprompt, "Please enter issue title\n(Title is mandatory)\n\nCtrl + c to cancel")
 	}
-	if issueEd, err := g.SetView("issueEd", maxX/4, (maxY/3)+(maxY/6), maxX-(maxX/4), maxY-(maxY/3)); err != nil {
+	if issueEd, err := g.SetView("issueEd", maxX/4, maxY/3, maxX-(maxX/4), maxY-(maxY/6)); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
@@ -2158,20 +2162,21 @@ func newIssue(g *gocui.Gui, v *gocui.View) error {
 
 //nextEntry is used to cycle through each option in the newIssue creation process
 func nextEntry(g *gocui.Gui, v *gocui.View) error {
-	issueEd, err := g.View("issueEd")
-	if err != nil {
-		return err
-	}
+	onceThrough := false
 	issueprompt, err := g.View("issueprompt")
 	if err != nil {
 		return err
 	}
-	ox, oy := issueEd.Origin()
-	if err := issueEd.SetCursor(ox, oy); err != nil {
-		return err
-	}
 	switch {
 	case entryCount == 0:
+		issueEd, err := g.View("issueEd")
+		if err != nil {
+			return err
+		}
+		ox, oy := issueEd.Origin()
+		if err := issueEd.SetCursor(ox, oy); err != nil {
+			return err
+		}
 		if len(issueEd.Buffer()) >= 2 {
 			tempIssueTitle = issueEd.Buffer()[:len(issueEd.Buffer())-2]
 		}
@@ -2184,6 +2189,14 @@ func nextEntry(g *gocui.Gui, v *gocui.View) error {
 			entryCount++
 		}
 	case entryCount == 1:
+		issueEd, err := g.View("issueEd")
+		if err != nil {
+			return err
+		}
+		ox, oy := issueEd.Origin()
+		if err := issueEd.SetCursor(ox, oy); err != nil {
+			return err
+		}
 		if issueEd.Buffer() != "" {
 			tempIssueBody = issueEd.Buffer()[:len(issueEd.Buffer())-2]
 		}
@@ -2192,23 +2205,116 @@ func nextEntry(g *gocui.Gui, v *gocui.View) error {
 		issueEd.Clear()
 		entryCount++
 	case entryCount == 2:
-		if issueEd.Buffer() != "" {
-			tempIssueAssignee = issueEd.Buffer()[:len(issueEd.Buffer())-2]
+		if !onceThrough {
+			issueEd, err := g.View("issueEd")
+			if err != nil {
+				return err
+			}
+			ox, oy := issueEd.Origin()
+			if err := issueEd.SetCursor(ox, oy); err != nil {
+				return err
+			}
+			if issueEd.Buffer() != "" {
+				tempIssueAssignee = issueEd.Buffer()[:len(issueEd.Buffer())-2]
+			}
+			issueEd.Clear()
+			if err := g.DeleteView("issueEd"); err != nil {
+				return err
+			}
 		}
 		issueprompt.Clear()
-		fmt.Fprintln(issueprompt, "Please enter issue labels, label titles are comma separated\n(Leave blank for no labels)\n\nCtrl + c to cancel")
-		issueEd.Clear()
+		fmt.Fprintln(issueprompt, "Please choose a label(s) to assign to this issue.\n(Choose a blank line for no labels)\n\nCtrl + c to cancel")
+		maxX, maxY := g.Size()
+		selectionPane, err := g.SetView("selectionPane", maxX/4, maxY/3, maxX/2, maxY-(maxY/6))
+		if err != nil {
+			if err != gocui.ErrUnknownView {
+				return err
+			}
+			selectionPane.Highlight = true
+			fmt.Fprintln(selectionPane, "\t")
+			for i := 0; i < len(labelList); i++ {
+				fmt.Fprintln(selectionPane, *labelList[i].Name)
+			}
+		}
+		if _, err := g.SetView("selectionDisplay", maxX/2, maxY/3, maxX-(maxX/4), maxY-(maxY/6)); err != nil {
+			if err != gocui.ErrUnknownView {
+				return err
+			}
+		}
+		if err := g.SetCurrentView("selectionPane"); err != nil {
+			return err
+		}
 		entryCount++
 	case entryCount == 3:
-		if issueEd.Buffer() != "" {
-			tempIssueLabels = strings.Split(issueEd.Buffer()[:len(issueEd.Buffer())-2], ",")
+		onceThrough = true
+		selectionPane, err := g.View("selectionPane")
+		if err != nil {
+			return err
+		}
+		_, cy := selectionPane.Cursor()
+		selection, err := selectionPane.Line(cy)
+		if err != nil {
+			return err
+		}
+		if err := g.SetCurrentView("selectionPane"); err != nil {
+			return err
+		}
+		selectionDisplay, err := g.View("selectionDisplay")
+		if err != nil {
+			return err
+		}
+		if selection != "\t" {
+			tempIssueLabels = append(tempIssueLabels, selection)
+			for i := 0; i < len(tempIssueLabels); i++ {
+				fmt.Fprintln(selectionDisplay, tempIssueLabels[i])
+			}
+			issueprompt.Clear()
+			fmt.Fprintln(issueprompt, "Add another label?")
+			selectionPane.Clear()
+			fmt.Fprintln(selectionPane, "Yes")
+			fmt.Fprintln(selectionPane, "No")
+			selectionPane.SetOrigin(0, 0)
+			selectionPane.SetCursor(0, 0)
+			another, err := selectionPane.Line(0)
+			if err != nil {
+				return err
+			}
+			if another == "Yes" {
+				entryCount--
+			} else {
+				entryCount++
+			}
+		} else {
+			entryCount++
+		}
+
+	case entryCount == 4:
+		selectionPane, err := g.View("selectionPane")
+		if err != nil {
+			return err
+		}
+		maxX, maxY := g.Size()
+		if err := g.DeleteView("selectionDisplay"); err != nil {
+			return err
 		}
 		issueprompt.Clear()
 		fmt.Fprintln(issueprompt, "Press enter to confirm entries and write out")
 		fmt.Fprintln(issueprompt, "")
 		fmt.Fprintln(issueprompt, "Press Ctrl + c to cancel")
-		issueEd.Clear()
-		issueEd.Editable = false
+		selectionPane.Clear()
+		if err := g.DeleteView("selectionPane"); err != nil {
+			return err
+		}
+		issueEd, err := g.SetView("issueEd", maxX/4, maxY/3, maxX-(maxX/4), maxY-(maxY/6))
+		if err != nil {
+			if err != gocui.ErrUnknownView {
+				return err
+			}
+			issueEd.Editable = false
+		}
+		if err := g.SetCurrentView("issueEd"); err != nil {
+			return err
+		}
 		fmt.Fprintln(issueEd, "Title: "+tempIssueTitle)
 		fmt.Fprintln(issueEd, "Body: "+tempIssueBody)
 		fmt.Fprintln(issueEd, "Assignee: "+tempIssueAssignee)
@@ -2221,7 +2327,7 @@ func nextEntry(g *gocui.Gui, v *gocui.View) error {
 			}
 		}
 		entryCount++
-	case entryCount == 4:
+	case entryCount == 5:
 		_, err := gitissue.MakeIssue(getRepo(), tempIssueTitle, tempIssueBody, tempIssueAssignee, 0, tempIssueLabels)
 		if err != nil {
 			return err
@@ -2251,6 +2357,28 @@ func cancel(g *gocui.Gui, v *gocui.View) error {
 			return err
 		}
 		entryCount = 0
+		tempIssueTitle = ""
+		tempIssueBody = ""
+		tempIssueAssignee = ""
+		tempIssueLabels = make([]string, 0)
+		if err := g.SetCurrentView(previousView.Name()); err != nil {
+			return err
+		}
+	} else if (g.CurrentView()).Name() == "selectionPane" {
+		if err := g.DeleteView("issueprompt"); err != nil {
+			return err
+		}
+		if err := g.DeleteView("selectionPane"); err != nil {
+			return err
+		}
+		if err := g.DeleteView("selectionDisplay"); err != nil {
+			return err
+		}
+		entryCount = 0
+		tempIssueTitle = ""
+		tempIssueBody = ""
+		tempIssueAssignee = ""
+		tempIssueLabels = make([]string, 0)
 		if err := g.SetCurrentView(previousView.Name()); err != nil {
 			return err
 		}
